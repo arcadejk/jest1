7c179f32ecd236cf74e1b2c0362ebd58d36dc605
Arcade Kakpo <arcadej@hotmail.fr>
2024-08-11 20:33:56 +0000

    Write just enough code to get the test passed

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -3,10 +3,10 @@ const addition = require("../calc");
 describe("calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22), toBe(42));
-        });
+            expect(addition(20, 22)).toBe(42);
+        })
     });
-    describe("Sustraction function", () => {
+    describe("Subtraction function", () => {
 
     });
     describe("Multiply function", () => {
