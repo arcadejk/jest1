09039421ce3202035cd7287ad4befaa7429cc42e
Arcade Kakpo <arcadej@hotmail.fr>
2024-08-17 09:53:20 +0000

    Set data-listener to true and create test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -27,7 +27,10 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains correct the ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
-    });   
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
 });
     
 describe("newGame works correctly", () => {
@@ -49,6 +52,12 @@ describe("newGame works correctly", () => {
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
+    });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
     });
 });
 
