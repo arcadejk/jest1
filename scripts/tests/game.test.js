fbac9bc8ad0b4086ea56a86e5a7a938b565babde
Arcade Kakpo <arcadej@hotmail.fr>
2024-08-18 14:35:03 +0000

    Create lastButton and turnInProgress and the tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -33,6 +33,14 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key is false", () => {
+        expect("turnInProgress" in game).toBe(true);
 });
     
 describe("newGame works correctly", () => {
@@ -103,5 +111,11 @@ describe("gameplay works correctly", () => {
     test("Should toggle turnInProgress to true", () => {
         showTurns();
         expect(game.turnInProgress).toBe(true);
+    });
+     test("clicking during computer sequence should fall", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
     });
 });
